650df277c26f6311242b9c2dd4e66eb39e085c09
gwydion75 <gwydion75@gmail.com>
2016-11-23 10:04:29 -0800

    Update role.roadman.js

--- role.roadman.js
+++ role.roadman.js
@@ -12,53 +12,61 @@ var roleRoadman = {
             creep.say('Roading');
         }
 
-        if (!creep.memory.roading) {
-            if (!creep.memory.south) {
-                var Container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] >0
-                })
-                if(creep.withdraw(Container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(Container);
-                }
-            }
-            else {
-                if(creep.room.name != southRoom) {
-                    creep.moveTo(creep.pos.findClosestByPath(creep.room.findExitTo(southRoom)));
-                }
-                else if(creep.room.name == southRoom) {
-                    if(creep.carry.energy < creep.carryCapacity) {
-                        var dropenergy = creep.pos.findClosestByPath(FIND_DROPPED_ENERGY, {
-                        filter: (d) => {return (d.resourceType == RESOURCE_ENERGY)}});
-                        if (creep.pickup(dropenergy) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(dropenergy);
-                        }
-                    }
-                }
-            }
+        if (!creep.memory.roading && !creep.memory.south) {
+           var Container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+           filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] >0
+           })
+           if(creep.withdraw(Container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+               creep.moveTo(Container);
+           }
         }
-        if (creep.memory.roading) {
+        else if (!creep.memory.roading && creep.memory.south) {
+           if(creep.room.name != southRoom) {
+               creep.moveTo(creep.pos.findClosestByPath(creep.room.findExitTo(southRoom)));
+           }
+           else if(creep.room.name == southRoom) {
+               if(creep.carry.energy < creep.carryCapacity) {
+                   var dropenergy = creep.pos.findClosestByPath(FIND_DROPPED_ENERGY, {
+                   filter: (d) => {return (d.resourceType == RESOURCE_ENERGY)}});
+                   if (creep.pickup(dropenergy) == ERR_NOT_IN_RANGE) {
+                       creep.moveTo(dropenergy);
+                   }
+               }
+           }
+        }
+        else if (creep.memory.roading && creep.memory.south) {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(targets[0]);
             }
             if (targets.length == 0) {
-                var repairroad = creep.room.find(FIND_STRUCTURES, { 
+                var repairSouthRoad = creep.room.find(FIND_STRUCTURES, { 
                     filter: (structure) => { 
                         return (structure.structureType == STRUCTURE_ROAD && structure.hits < 1600 && structure.hits > 0)
                     }
                 });
-                if ((repairroad.length > 0) && (creep.carry.energy > 0)) {
-                    //var rndRepairRoad = Math.floor(Math.random() * repairroad.length);
-                    if (creep.repair(repairroad[0]) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(repairroad[0]);
+                if ((repairSouthRoad.length > 0) && (creep.carry.energy > 0)) {
+                    if (creep.repair(repairSouthRoad[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(repairSouthRoad[0]);
                     }
                 }
+                else if (creep.memory.south && repairroad.length = 0) {
+                    creep.moveTo(Game.flags.Rally1);
+                }
             }
-            else if (!creep.memory.south) {
+        }
+        else if (creep.memory.roading && !creep.memory.south) {
+            var repairNorthRoad = creep.room.find(FIND_STRUCTURES, { 
+            filter: (structure) => { 
+                return (structure.structureType == STRUCTURE_ROAD && structure.hits < 1600 && structure.hits > 0)
+                }
+            });
+            if ((repairNorthRoad.length > 0) && (creep.carry.energy > 0)) {
+                if (creep.repair(repairNorthRoad[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(repairNorthRoad[0]);
+                }
+            else if (repairNorthRoad.length = 0) {
                 creep.moveTo(Game.flags.Rally2);
-            }
-            else {
-                creep.moveTo(Game.flags.Rally1);
             }
         }
     }
