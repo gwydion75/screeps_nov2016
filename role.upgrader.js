f81de6f57f1463eb94e787ac9f08f83737a4f65c
gwydion75 <gwydion75@gmail.com>
2016-11-22 21:18:02 -0800

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -11,26 +11,37 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
+        if(!creep.memory.gathering && creep.carry.energy == 0) {
             creep.memory.upgrading = false;
-            creep.say('harvesting');
+            creep.memory.building = false;
+            creep.memory.gathering = true;
+            creep.say('Gathering');
         }
-        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+        if(creep.memory.gathering && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.building = true;
+            creep.memory.gathering = false;
+            creep.memory.upgrading = false;
+            creep.say('Building');
+        }
+        if(creep.memory.building && creep.carry.energy == creep.carryCapacity && targets.length) {
             creep.memory.upgrading = true;
-            creep.say('upgrading');
+            creep.memory.gathering = false;
+            creep.memory.building = false;
+            creep.say('Upgrading');
         }
-
+        if(targets.length && creep.memory.building) {
+            if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0]);
+            }
+        }
         if(creep.memory.upgrading) {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
         }
-        else {
-            /*var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }*/
+        if(creep.memory.gathering) {
             var Container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] >0
             })
