7d457462d1f8965a6266ae9d9c86bd9e62c6f614
gwydion75 <gwydion75@gmail.com>
2016-11-22 20:49:31 -0800

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -25,6 +25,14 @@ var roleHarvester = {
             }
         }
         else {
+            var dropenergy = creep.pos.findClosestByPath(FIND_DROPPED_ENERGY, {
+            filter: (d) => {return (d.resourceType == RESOURCE_ENERGY)}});
+            if (dropenergy.length > 0) {
+                creep.say('Found ' + dropenergy.length + 'e');
+                if (creep.pickup(dropenergy) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(dropenergy);
+                }
+            }
             var Container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] >0
             })
