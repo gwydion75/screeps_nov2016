3613b8e8a8eebb87a056d486f33d8b683c364297
gwydion75 <gwydion75@gmail.com>
2016-11-22 21:08:52 -0800

    Update main.js

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ module.exports.loop = function () {
             console.log('Spawning new builder: ' + newName);
         }
         if(upgraders.length < 5) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], undefined, {role: 'upgrader'});
+            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'upgrader'});
             console.log('Spawning new upgrader: ' + newName);
         }
         if(harvesters.length < 2) {
