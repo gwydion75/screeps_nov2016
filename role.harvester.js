7fa0da150b8faeca39141cd974b921e83d972732
gwydion75 <gwydion75@gmail.com>
2016-11-22 20:39:33 -0800

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -6,23 +6,53 @@
  * var mod = require('role.harvester');
  * mod.thing == 'a thing'; // true
  */
+/*var roleWallman = {
+    run: function(creep) {
+        if(creep.memory.walling && creep.carry.energy == 0) {
+            creep.memory.walling = false;
+            creep.say('Fueling');
+        }
+        if(!creep.memory.walling && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.walling = true;
+            creep.say('Walling');
+        }
 
+        if (!creep.memory.walling) {
+            var Container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] >0
+            })
+            if(creep.withdraw(Container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Container);
+            }
+        }
+        if (creep.memory.walling) {
+            var repairwall = creep.room.find(FIND_STRUCTURES, { 
+                filter: (structure) => { 
+                    return (structure.structureType == STRUCTURE_RAMPART && structure.hits < 1000 && structure.hits > 0 || structure.hits < 1000 && structure.hits > 0 && structure.structureType == STRUCTURE_WALL)
+                }
+            });
+            if ((repairwall.length > 0) && (creep.carry.energy > 0)) {
+                var rndRepairWall = Math.floor(Math.random() * repairwall.length);
+                if (creep.repair(repairwall[rndRepairWall]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(repairwall[rndRepairWall]);
+                }
+            }
+        }
+    }
+}*/
 var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            /*creep.moveTo(28,24);
-            creep.harvest(Game.getObjectById('57ef9dfe86f108ae6e60e9f7'));
-            creep.harvest(creep.room.lookAt(27,23));
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }*/
-             /*var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }*/
+        if(creep.memory.gathering && creep.carry.energy == 0) {
+            creep.memory.gathering = true;
+            creep.say('Gathering');
+        }
+        if(!creep.memory.gathering && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.gathering = false;
+            creep.say('Delivering');
+        }
+        if(creep.memory.gathering) {
             var Container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] >0
             })
